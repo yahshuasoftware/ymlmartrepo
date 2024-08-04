b10d1d88371387615e3b1a8b3e2da29cf707ea10
ArjunAHanwate <118721237+ArjunAHanwate@users.noreply.github.com>
2024-08-04 20:09:39 +0530

    modifieed footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
+// App.js
 import './App.css';
 import CategoryBar from './Components/HeroCategory/Categories';
 import Navbar from './Components/Navbar/Navbar';
-import Login from './Components/Login/login'
-import Register  from './Components/Register/register';
-import Footer from './Components/Footer/footer'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login/login';
+import Register from './Components/Register/register';
+import Footer from './Components/Footer/footer';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
+// Main App Component
 function App() {
+  const location = useLocation();
+
   return (
     <>
-    <Router>
-      <Navbar></Navbar>
-      <CategoryBar></CategoryBar>
+      {location.pathname !== '/login' && location.pathname !== '/register' && <Navbar />}
+      <CategoryBar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-      <Footer></Footer>
-  </Router>
-  </>
+      <Footer />
+    </>
   );
 }
+
+
 
 export default App;
--- frontend/src/Components/Footer/footer.js
+++ frontend/src/Components/Footer/footer.js
@@ -1,76 +1,137 @@
-import React from 'react'
-import '../Footer/footer.css'
+import React from "react";
+import { FaInstagram, FaTwitter, FaLinkedin, FaFacebook } from "react-icons/fa";
+import "../Footer/footer.css";
 
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div className="footer">
       <div className="top">
+        <div className="footer-top-img">
+          <a href="/about" >
+            <div className="company-img">
+              <img src="comp1.png" alt="" />
+            </div>
+            <p>About Company</p>
+          </a>
 
+          <a href="contact">
+            <div className="company-img">
+              <img src="contact.png" alt="" />
+            </div>
+            <p>Contact Us</p>
+          </a>
+
+          <a href="/askquestion">
+            <div className="company-img">
+              <img src="comp2.png" alt="" />
+            </div>
+            <p>Ask Question</p>
+          </a>
+        </div>
       </div>
       <div className="bottom">
         <div className="horizantal">
-            <div className="Download-section">
-                <img className='logo' src="logo.png" alt="" />
-                <h2>Download our app</h2>
-                <div className='images'>
-                    <img className='img1' src="Google-play.png" alt="img" />
-                    <img className='img2' src="App-store.png" alt="img" />
-                </div>
+          <div className="Download-section">
+            <div className="vertical">
+
+            <a href="/">
+            <img className="logo" src="logo.png" alt="" />
+            </a>
+            
+            <h2>Download our app</h2>
+            <div className="images">
+                <a href="https://play.google.com/"><img className="img1" src="Google-play.png" alt="img"/></a>
+                <a href="https://www.apple.com/in/app-store/"><img className="img2" src="App-store.png" alt="img"/></a>
+              
             </div>
-            <div className="Special-section">
+            </div>
+          </div>
+          <div className="Special-section">
             <h3>Special</h3>
-            <a href=''>Featured Products</a>
-            <a href=''>Latest Products</a>
-            <a href=''>Best Selling Products</a>
-            <a href=''>Top Rated Products</a>
-            </div>
-            <div className="Account-shipping">
+            <a href="">Featured Products</a>
+            <a href="">Latest Products</a>
+            <a href="">Best Selling Products</a>
+            <a href="">Top Rated Products</a>
+          </div>
+          <div className="Account-shipping">
             <h3> Account and Shipping Information</h3>
-            <a href=''>Profile Information</a>
-            <a href='/wishlist'>Wishlist</a>
-            <a href=''>Track Order</a>
-            <a href=''>Refund Policy</a>
-            <a href=''>Return Policy</a>
-            <a href=''>Cancellation Policy</a>
-            </div>
-            <div className="Newsletter">
+            <a href="">Profile Information</a>
+            <a href="/wishlist">Wishlist</a>
+            <a href="">Track Order</a>
+            <a href="">Refund Policy</a>
+            <a href="">Return Policy</a>
+            <a href="">Cancellation Policy</a>
+          </div>
+          <div className="Newsletter">
             <h3>Newsletter</h3>
             <p>Subscribe to our new channel to receive the latest updates.</p>
             <form action="">
-             <input className='text' type="email" placeholder='Email' name="" id="" />
-            <input className='btn' type="button" value="Subscribe"/>
+              <input
+                className="text"
+                type="email"
+                placeholder="Email"
+                name=""
+                id=""
+              />
+              <input className="btn" type="button" value="Subscribe" />
             </form>
-            
-            </div>
+          </div>
         </div>
 
         <div className="address">
-            <div className='left'>
-
+          <div className="left"></div>
+          <div className="right">
+            <div className="up">
+              <p>Start a conversation</p>
+              <p>Address</p>
             </div>
-            <div className='right'>
-                <div className='up'>
-                    <p>Start a conversation</p>
-                    <p>Address</p>
-                </div>
-                <div className='down'>
-                    <p>+91-9766440296</p>
-                    <p>support@ymlmart.com</p>
-                    <p>+91-9766440296</p>
-                    <p>Pune, Maharashtra</p>
-                </div>
+            <div className="down">
+              <p>+91-8888888888</p>
+              <p>support@ymlmart.com</p>
+              <p>+91-8888888888</p>
+              <p>Pune, Maharashtra</p>
             </div>
+          </div>
         </div>
 
         <div className="socials">
-            <p>&copy; 2024 | Yahshua Marketing Limited. All rights reserved.</p>
-            <div className="social-icons">Social Icons</div>
-            <a>Terms and Conditions</a>
-            <a>Privacy Policy</a>
+          <p>&copy; 2024 | Yahshua Marketing Limited.</p>
+          <div className="social-icons">
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram size={20} />
+            </a>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter size={20} />
+            </a>
+            <a
+              href="https://www.linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin size={20} />
+            </a>
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook size={20} />
+            </a>
+          </div>
+          <a>Terms and Conditions</a>
+          <a>Privacy Policy</a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
